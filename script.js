422220ca7e06230793b4e82b0b1aa7dc62b36e00
Tropicaljittai <93302274+Tropicaljittai@users.noreply.github.com>
2023-01-07 19:02:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const categoryTitle = document.querySelectorAll(".category-title");
+const allCategoryPosts = document.querySelectorAll(".all");
+var videoPlayer = document.getElementById("VideoPlayer");
+var myVideo = document.getElementById("myVideo");
+var allPosts = document.getElementsByClassName("all");
+
+function stopVideo() {
+  videoPlayer.style.display = "none";
+}
+
+function playVideo(file) {
+  myVideo.src = file;
+  videoPlayer.style.display = "block";
+}
+
+for (let i = 0; i < categoryTitle.length; i++) {
+  categoryTitle[i].addEventListener("click", filterPosts.bind(this, categoryTitle[i]));
+}
+
+function filterPosts(item) {
+  changeActivePosition(item);
+  for (let i = 0; i < allCategoryPosts.length; i++) {
+    if (allCategoryPosts[i].classList.contains(item.attributes.id.value)) {
+      allCategoryPosts[i].style.display = "block";
+    } else {
+      allCategoryPosts[i].style.display = "none";
+    }
+  }
+}
+
+function changeActivePosition(activeItem) {
+  for (let i = 0; i < categoryTitle.length; i++) {
+    categoryTitle[i].classList.remove("active");
+  }
+  activeItem.classList.add("active");
+}
